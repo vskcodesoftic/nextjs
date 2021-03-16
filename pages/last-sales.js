a7a829ba2294ebb8ffc72f501911e8451e1712cf
vskcodesoftic <vegisiva@codesoftic.com>
2021-03-16 16:24:35 +0530

    added firebase fetch using swr hook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,13 +1,15 @@
 import React, { useEffect , useState } from 'react'
+import useSWR from 'swr'
 
 const LastSalesPage = () => {
-    const [sales, setSales] = useState('')
-    const [isLoading, setisLoading] = useState(false)
-    useEffect(() => {
-        setisLoading(true)
-       fetch('https://nextjs-c5196-default-rtdb.firebaseio.com/sales.json').then(res => res.json())
-       .then(data => {
-           const transfromedSales = []
+     const [sales, setSales] = useState('')
+    // const [isLoading, setisLoading] = useState(false)
+   
+    const { data, error } =  useSWR('https://nextjs-c5196-default-rtdb.firebaseio.com/sales.json');
+   
+   useEffect(() => {
+    if(data) {
+          const transfromedSales = []
 
            for(const key in data) {
                transfromedSales.push({
@@ -15,19 +17,38 @@ const LastSalesPage = () => {
                    username : data[key].username,
                    volume : data[key].volume
                })
-           }
+           } 
+           setSales(transfromedSales)
+    }
+   
+   }, [data])
 
-           setSales(transfromedSales);
-           setisLoading(false);
-       })
-    }, [])
 
-    if(isLoading) {
-        return <p>Loading....</p>
+    // useEffect(() => {
+    //     setisLoading(true)
+    //    fetch('https://nextjs-c5196-default-rtdb.firebaseio.com/sales.json').then(res => res.json())
+    //    .then(data => {
+    //        const transfromedSales = []
+
+    //        for(const key in data) {
+    //            transfromedSales.push({
+    //                id: key, 
+    //                username : data[key].username,
+    //                volume : data[key].volume
+    //            })
+    //        }
+
+    //        setSales(transfromedSales);
+    //        setisLoading(false);
+    //    })
+    // }, [])
+
+    if(error) {
+        return <p>Failed to load .</p>
     }
 
-    if(!sales) {
-        return <p>NoData yet</p>
+    if(!data || !sales) {
+        return <p>Loading ... </p>
     }
     return (
         <ul>
