43ab08f3d3a2d9d5c16e34641cac4f6f22a950c8
vskcodesoftic <vegisiva@codesoftic.com>
2021-03-16 13:40:03 +0530

    dyanamic pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import fs from 'fs/promises';
 import path from 'path';
-
+import Link from 'next/link'
 function HomePage(props) {
   const { products } = props;
   return (
     <ul>
-     {products.map(product => <li key={product.id}>
-       {product.title}
-     </li>)}
+           {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
     </ul>
   );
 }
