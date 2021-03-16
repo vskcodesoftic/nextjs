100fe748e9c4ce69b0a21f7cdd878ebe2ad5432d
vskcodesoftic <vegisiva@codesoftic.com>
2021-03-16 10:43:07 +0530

    get static props

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+function HomePage(props) {
+  const { products } = props;
+  return (
+    <ul>
+     {products.map(product => <li key={product.id}>
+       {product.title}
+     </li>)}
+    </ul>
+  );
+}
+
+export async function getStaticProps(){
+  return { props : {
+    products : [{ id: 'p1', title : 'product one'}]
+  }};
+}
+
+export default HomePage;
