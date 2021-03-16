fe41fc70be389c08519b9d509c0b4d3ae77e6c47
vskcodesoftic <vegisiva@codesoftic.com>
2021-03-16 16:33:55 +0530

    added swr hook and made sales page to be prendred page

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import React, { useEffect , useState } from 'react'
 import useSWR from 'swr'
 
-const LastSalesPage = () => {
-     const [sales, setSales] = useState('')
+const LastSalesPage = (props) => {
+     const [sales, setSales] = useState(props.sales)
     // const [isLoading, setisLoading] = useState(false)
    
     const { data, error } =  useSWR('https://nextjs-c5196-default-rtdb.firebaseio.com/sales.json');
@@ -47,7 +47,7 @@ const LastSalesPage = () => {
         return <p>Failed to load .</p>
     }
 
-    if(!data || !sales) {
+    if(!data && !sales) {
         return <p>Loading ... </p>
     }
     return (
@@ -57,6 +57,25 @@ const LastSalesPage = () => {
             </li>)}
         </ul>
     )
+}
+
+export async function getStaticProps() {
+   const response = await fetch('https://nextjs-c5196-default-rtdb.firebaseio.com/sales.json')
+   const data = await response.json();    
+
+           const transfromedSales = []
+
+           for(const key in data) {
+               transfromedSales.push({
+                   id: key, 
+                   username : data[key].username,
+                   volume : data[key].volume
+               })
+           }
+         return { props : {
+             sales : transfromedSales
+         } , revalidate : 10}
+    
 }
 
 export default LastSalesPage
